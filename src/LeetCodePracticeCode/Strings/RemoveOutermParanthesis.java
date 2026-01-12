a6806a8ed268ab83af08c9657dab77cebb42f488
AmanKumarChoubey <choubeyaman279@gmail.com>
2026-01-12 23:17:33 +0530

    Solve Problem

---
+++ src/LeetCodePracticeCode/Strings/RemoveOutermParanthesis.java
@@ -0,0 +1,27 @@
+package LeetCodePracticeCode.Strings;
+
+import java.util.Stack;
+
+public class RemoveOutermParanthesis {
+    public String removeOuterParentheses(String s) {
+        Stack<Character> stack=new Stack<>();
+
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('){
+                if(stack.size()>0){
+                    sb.append(ch);
+                }
+                stack.push(ch);
+            }
+            else{
+                stack.pop();
+                if(stack.size()>0){
+                    sb.append(ch);
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
