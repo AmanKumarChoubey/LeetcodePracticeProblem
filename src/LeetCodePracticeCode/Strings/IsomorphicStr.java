a717f33bf10cf4ca1f70aa5078f05dcc2c77b6c9
AmanKumarChoubey <choubeyaman279@gmail.com>
2026-01-13 22:54:30 +0530

    Solve Problem

---
+++ src/LeetCodePracticeCode/Strings/IsomorphicStr.java
@@ -0,0 +1,24 @@
+package LeetCodePracticeCode.Strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IsomorphicStr {
+    public boolean isIsomorphic(String s, String t) {
+        int n = s.length(), m = t.length();
+        Map<Character, Character> map = new HashMap<>();
+        if (n != m) return false;
+        for (int i = 0; i < n; i++) {
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+            if (map.containsKey(ch1)) {
+                if (map.get(ch1) != ch2) return false;
+            } else if (map.containsValue(ch2)) {
+                return false;
+            } else {
+                map.put(ch1, ch2);
+            }
+        }
+        return true;
+    }
+}
