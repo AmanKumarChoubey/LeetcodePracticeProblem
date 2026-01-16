22330916c3b0a899af1df56284d22339bb8efbab
Aman Choubey <122197295+AmanKumarChoubey@users.noreply.github.com>
2026-01-16 22:41:21 +0530

    Create KokoEatBanana.java

---
+++ src/LeetCodePracticeCode/BinarySearch/KokoEatBanana.java
@@ -0,0 +1,31 @@
+class KokoEatBanana{
+  public int minEatingSpeed(int[] piles, int h) {
+        int maxLen=Integer.MIN_VALUE, ans=0;
+        for(int i=0;i<piles.length;i++){
+            if(piles[i]>maxLen){
+                maxLen=piles[i];
+            }
+        }
+        
+        int low=1, high=maxLen;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int hours=solver(piles,mid);
+            if(hours<=h){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    public int solver(int piles[],int mid){
+        int time=0;
+        for(int i=0;i<piles.length;i++){
+            time+=Math.ceil((double)piles[i]/(double)mid);
+        }
+        return time;
+    }
+}
