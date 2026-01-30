b1754270597c3655aee4c6d2482db5fb353593da
Aman Choubey <122197295+AmanKumarChoubey@users.noreply.github.com>
2026-01-30 22:58:10 +0530

    Create CountingBits.java

---
+++ src/LeetCodePracticeCode/CountingBits.java
@@ -0,0 +1,17 @@
+class CountingBits{
+  public int[] countBits(int n) {
+        int[] ans=new int[n+1];
+        if(n<=0) return ans;
+        ans[0]=0;
+
+        for(int i=1;i<ans.length;i++){
+            int count=0, num=i;
+            while(num>0){
+                count+=(num&1);
+                num>>=1;
+            }
+            ans[i]=count;
+        }
+        return ans;
+    }
+}
