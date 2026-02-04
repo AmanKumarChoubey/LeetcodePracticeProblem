40d3b8a8da2d446bdccaba9357553736a78fa338
Aman Choubey <122197295+AmanKumarChoubey@users.noreply.github.com>
2026-02-04 22:31:37 +0530

    Update SingleNum2.java

--- src/LeetCodePracticeCode/SingleNum2.java
+++ src/LeetCodePracticeCode/SingleNum2.java
@@ -1,5 +1,6 @@
 class SingleNum2{
   public int singleNumber(int[] nums) {
+    // ~ represent Not Operator
         int ones = 0, twos = 0;
         for (int ele : nums) {
             ones = (ones ^ ele) & (~twos);
