21299ab805b37101ef39dd09e2e21d56ab66aa08
Aman Choubey <122197295+AmanKumarChoubey@users.noreply.github.com>
2026-02-05 23:30:30 +0530

    Update MinBitwiseArr1.java

--- src/LeetCodePracticeCode/MinBitwiseArr1.java
+++ src/LeetCodePracticeCode/MinBitwiseArr1.java
@@ -4,14 +4,14 @@ class MinBitwiseArr1{
        int n=nums.size(), ind=0;
        int ans[]=new int[n];
        for(int i=0;i<n;i++){
-          int num=nums.get(i), in=-1;
+          int num=nums.get(i), index=-1;
           for(int j=0;j<num;j++){
             if((j|(j+1))==num){
-                in=j;
+                index=j;
                 break;
             }
           }
-          ans[ind++]=in;
+          ans[ind++]=index;
        }
        return ans;
     }
