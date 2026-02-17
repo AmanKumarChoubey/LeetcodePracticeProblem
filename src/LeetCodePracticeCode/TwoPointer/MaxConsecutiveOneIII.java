4241d84060905ba72161d1401b6a9b0187825d98
Aman Choubey <122197295+AmanKumarChoubey@users.noreply.github.com>
2026-02-17 23:04:30 +0530

    Create MaxConsecutiveOneIII.java

---
+++ src/LeetCodePracticeCode/TwoPointer/MaxConsecutiveOneIII.java
@@ -0,0 +1,18 @@
+class MaxConsecutiveOneIII{
+  public int longestOnes(int[] nums, int k) {
+        // Subarray Problem deals with two pointer and sliding window
+        int left=0, right=0, n=nums.length, maxLen=Integer.MIN_VALUE, zeroCnt=0;
+        while(right<n){
+            if(nums[right]==0){
+                zeroCnt++;
+            }
+            while(zeroCnt>k){
+                if(nums[left]==0) zeroCnt--;
+                left++;
+            }
+            maxLen=Math.max(maxLen,right-left+1);
+            right++;
+        }
+        return maxLen;
+    }
+}
