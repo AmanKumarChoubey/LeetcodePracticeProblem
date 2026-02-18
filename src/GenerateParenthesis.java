ebdd03c5a35e2ef5ae7faf9eac831e0fa2f8c4b1
Aman Choubey <122197295+AmanKumarChoubey@users.noreply.github.com>
2026-02-18 23:11:50 +0530

    Create GenerateParenthesis.java

---
+++ src/GenerateParenthesis.java
@@ -0,0 +1,20 @@
+class GenerateParenthesis{
+  public List<String> generateParenthesis(int n) {
+       List<String> ans=new ArrayList<>();
+       int open=n,close=n;
+       helper(ans,open,close,""); 
+       return ans;
+    }
+    public void helper(List<String> ans,int open,int close,String output){
+        if(open==0 && close==0){
+            ans.add(output);
+            return;
+        }
+        if(open!=0){
+            helper(ans,open-1,close,output+'(');
+        }
+        if(close>open){
+            helper(ans,open,close-1,output+')');
+        }
+    }
+}
