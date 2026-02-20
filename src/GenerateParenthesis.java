539db3b7ae7f87041b64aafe9b51e7bcd33087d3
Aman Choubey <122197295+AmanKumarChoubey@users.noreply.github.com>
2026-02-20 17:41:36 +0530

    Implemented by Own GenerateParenthesis.java

--- src/GenerateParenthesis.java
+++ src/GenerateParenthesis.java
@@ -1,21 +1,20 @@
 class GenerateParenthesis{
   public List<String> generateParenthesis(int n) {
     // Recursion Question
-       List<String> ans=new ArrayList<>();
-       int open=n,close=n;
-       helper(ans,open,close,""); 
-       return ans;
+      List<String> ans=new ArrayList<>();
+      solver(n,n,ans,"");
+      return ans;
+  }
+  public void solver(int openBrack,int closeBrack,List<String> ans,String temp){
+    if(openBrack==0 && closeBrack==0){
+      ans.add(temp);
+      return;
     }
-    public void helper(List<String> ans,int open,int close,String output){
-        if(open==0 && close==0){
-            ans.add(output);
-            return;
-        }
-        if(open!=0){
-            helper(ans,open-1,close,output+'(');
-        }
-        if(close>open){
-            helper(ans,open,close-1,output+')');
-        }
+    if(openBrack!=0){
+      solver(openBrack-1,closeBrack,ans,temp+'(');
     }
+    if(closeBrack>openBrack){
+      solver(openBrack,closeBrack-1,ans,temp+')');
+    }
+  }
 }
