b4d8957d8ebb736cfa8097ea67eb2c69387d2339
Aman Choubey <122197295+AmanKumarChoubey@users.noreply.github.com>
2026-02-20 18:17:37 +0530

    Update ReverseLL.java

--- src/LeetCodePracticeCode/LinkedList/ReverseLL.java
+++ src/LeetCodePracticeCode/LinkedList/ReverseLL.java
@@ -1,13 +1,24 @@
 class ReverseLL{
   public ListNode reverseList(ListNode head) {
         // code here
-        ListNode curr=head, prev=null;
-        while(curr!=null){
-            ListNode nextNode=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=nextNode;
-        } 
-        return prev;
+        // ListNode curr=head, prev=null;
+        // while(curr!=null){
+        //     ListNode nextNode=curr.next;
+        //     curr.next=prev;
+        //     prev=curr;
+        //     curr=nextNode;
+        // } 
+        // return prev;
+
+        // Implement with Recursion
+        return solver(head,null);
+    }
+  public ListNode solver(ListNode  curr, ListNode prev){
+        if(curr==null) 
+           return prev;
+
+        ListNode nextNode=curr.next;
+        curr.next=prev;
+        return solver(nextNode,curr);
     }
 }
