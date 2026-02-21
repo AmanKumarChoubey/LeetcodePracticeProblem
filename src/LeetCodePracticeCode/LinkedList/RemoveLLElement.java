566065631490cf3f518197f4127bf2f83743f71b
Aman Choubey <122197295+AmanKumarChoubey@users.noreply.github.com>
2026-02-21 23:57:20 +0530

    Update RemoveLLElement.java

--- src/LeetCodePracticeCode/LinkedList/RemoveLLElement.java
+++ src/LeetCodePracticeCode/LinkedList/RemoveLLElement.java
@@ -2,16 +2,21 @@ class RemoveLLElement{
    public ListNode removeElements(ListNode head, int val) {
         // code here
         // Through itration
-        while(head!=null && head.val==val) head=head.next;
-        ListNode temp=head;
-        while(temp!=null && temp.next!=null){
-            if(temp.next.val==val){
-                temp.next=temp.next.next;
-            }
-            else
-                temp=temp.next;
-        }
+        // while(head!=null && head.val==val) head=head.next;
+        // ListNode temp=head;
+        // while(temp!=null && temp.next!=null){
+        //     if(temp.next.val==val){
+        //         temp.next=temp.next.next;
+        //     }
+        //     else
+        //         temp=temp.next;
+        // }
        
-        return head;
+        // return head;
+
+      // Through Recursive Approach
+      if (head == null) return null;
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;
    }
 }
