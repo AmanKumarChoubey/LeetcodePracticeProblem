7f64a5011233ea2edf6e4df1eeed8e9e85be8842
Aman Choubey <122197295+AmanKumarChoubey@users.noreply.github.com>
2026-02-21 23:56:29 +0530

    Create RemoveLLElement.java

---
+++ src/LeetCodePracticeCode/LinkedList/RemoveLLElement.java
@@ -0,0 +1,17 @@
+class RemoveLLElement{
+   public ListNode removeElements(ListNode head, int val) {
+        // code here
+        // Through itration
+        while(head!=null && head.val==val) head=head.next;
+        ListNode temp=head;
+        while(temp!=null && temp.next!=null){
+            if(temp.next.val==val){
+                temp.next=temp.next.next;
+            }
+            else
+                temp=temp.next;
+        }
+       
+        return head;
+   }
+}
