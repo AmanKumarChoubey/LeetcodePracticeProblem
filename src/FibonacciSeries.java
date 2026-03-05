86ea6652fff68eb6429903235993d8103401faa2
Aman Choubey <122197295+AmanKumarChoubey@users.noreply.github.com>
2026-03-05 23:45:11 +0530

    Update FibonacciSeries.java

--- src/FibonacciSeries.java
+++ src/FibonacciSeries.java
@@ -1,6 +1,7 @@
 class FibonacciSeries{
   // Implement through Recusrion
   public int fib(int n) {
+    // Solve here
         if(n==0) return 0;
         if(n==1) return 1;
         return fib(n-1)+fib(n-2);
